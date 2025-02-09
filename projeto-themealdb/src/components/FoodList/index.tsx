9af6f9b853987f98d9c0ce3b890db72006f90dc6
Ágata Rafaela <agatadevv@gmail.com>
2025-02-09 11:58:58 -0300

    fix

--- projeto-themealdb/src/components/FoodList/index.tsx
+++ projeto-themealdb/src/components/FoodList/index.tsx
@@ -9,10 +9,17 @@ function FoodList() {
         <main>
             <Menu />
             <Banner />
-            
-            <FoodCarousel apiUrl="https://api-receitas-pi.vercel.app/receitas/todas" title="PARA VOCÊ" />
-            <FoodCarousel apiUrl="https://api-receitas-pi.vercel.app/receitas/tipo/salgado" title="PRATO PRINCIPAL" />
-            <FoodCarousel apiUrl="https://api-receitas-pi.vercel.app/receitas/tipo/doce" title="SOBREMESA" />
+            <div id="foodlist">
+                <FoodCarousel apiUrl="https://api-receitas-pi.vercel.app/receitas/todas" title="PARA VOCÊ" />
+            </div>
+
+            <div id="salty">
+                <FoodCarousel apiUrl="https://api-receitas-pi.vercel.app/receitas/tipo/salgado" title="PRATO PRINCIPAL" />
+            </div>
+
+            <div id="dessert">
+                <FoodCarousel apiUrl="https://api-receitas-pi.vercel.app/receitas/tipo/doce" title="SOBREMESA" />
+            </div>
         </main>
     );
 }
